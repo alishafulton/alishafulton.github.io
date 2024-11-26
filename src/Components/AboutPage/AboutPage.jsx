0c5a7d9ffaa7e41b7483e5890cedab508c817106
alishafulton <af.ui.design@gmail.com>
2024-11-25 20:07:11 -0600

    !

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -9,7 +9,7 @@ export default function AboutPage() {
       return (
         <div className="flex flex-col justify-center justify-items-center w-full ">
             <Blurb />
-            <div className="pt-3"><Compliments/></div>
+            <Compliments/>
         </div>
       );
     }
