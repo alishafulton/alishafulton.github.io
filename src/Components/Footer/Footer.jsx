fa4f9046265d547d98703b69afae97535b53e963
alishafulton <af.ui.design@gmail.com>
2024-11-26 10:12:30 -0600

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ return (
       
       <div className="flex justify-center justify-items-center py-5 bg-white border-black border-t border-opacity-10">
         <div className="flex flex-row font-mono text-sm text-center tracking-tighter text-black">
-          <Logo/>
+        <NavLink to="/tester/" className="group"><Logo/></NavLink>
         </div>
     </div>
         
