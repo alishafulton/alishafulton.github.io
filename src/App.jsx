e1efcb9210c2943aadcf48f6a659a591f5f9789b
alishafulton <af.ui.design@gmail.com>
2024-11-26 13:30:34 -0600

    AOS

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,18 @@ import Navbar from "./Components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import './index.css'
 import Footer from "./Components/Footer/Footer";
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 export default function App () {
+  useEffect(() => {
+    AOS.init({
+    duration: 1000, // Global animation duration
+    once: true, // Only once animation
+    });
+    }, []);
   return (
     <div className="flex flex-col min-h-screen justify-center justify-items-center">
       <div className="sticky top-0 z-50 flex justify-center items-center">
