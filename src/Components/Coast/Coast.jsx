1c73091f100e859082653e1029aa1affe9a0291d
alishafulton <af.ui.design@gmail.com>
2025-01-04 17:09:03 -0600

    Update Coast.jsx

--- src/Components/Coast/Coast.jsx
+++ src/Components/Coast/Coast.jsx
@@ -4,15 +4,15 @@ import CoastPrototype from './CoastPrototype';
 export default function Coast () {
 
     return (
-        <div className="flex w-full pt-8 justify-center bg-coast-logo bg-scroll bg-contain bg-top overflow-auto bg-no-repeat">
+        <div className="flex w-full pt-4 justify-center bg-coast-logo bg-scroll bg-contain bg-top overflow-auto bg-no-repeat">
           <div className="flex flex-col w-full h-screen" >
 
           <div className="flex flex-col justify-center py-96 ">
             <div className="sm:text-4xl lg:text-8xl font-coast flex justify-center ">COAST</div>
-            <div className="flex justify-center text-sm font-sans w-36 bg-black bg-opacity-20 border-black border-2 rounded-lg px-4">UX/UI Case Study</div>
-          </div>
+            <div className="flex justify-center"><div className="flex justify-center text-sm font-sans w-36 bg-black bg-opacity-20 border-black border-2 rounded-lg px-4">UX/UI Case Study</div>
+          </div></div>
 
-          <CoastPrototype data-aos="slide-up"/>
+          <CoastPrototype className="flex w-full" data-aos="slide-up"/>
           </div>
 
 
