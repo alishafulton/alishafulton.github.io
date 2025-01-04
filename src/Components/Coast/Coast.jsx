a7195353f77af87f4e6fc8eb677cb62a650c1276
alishafulton <af.ui.design@gmail.com>
2025-01-04 16:39:12 -0600

    Update Coast.jsx

--- src/Components/Coast/Coast.jsx
+++ src/Components/Coast/Coast.jsx
@@ -37,13 +37,13 @@ export default function Coast () {
 
 
     return (
-      <div>
+      <div className="flex flex-col w-full">
     <div className="flex w-full justify-center bg-coast-logo bg-scroll bg-contain bg-top overflow-auto bg-no-repeat">
      
 
       <div className="flex flex-col w-full h-screen justify-center" >
 
-          <div className="flex justify-center">
+          <div className="flex justify-center py-80 ">
             <div className="sm:text-4xl lg:text-8xl font-coast justify-center ">COAST</div>
             <div className="flex justify-center text-sm font-sans w-36 bg-black bg-opacity-20 border-black border-2 rounded-lg px-4">UX/UI Case Study</div>
           </div>
