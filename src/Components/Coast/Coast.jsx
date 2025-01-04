dfc9baf1b94a3b982e0a1913cc5119b73a2df72f
alishafulton <af.ui.design@gmail.com>
2025-01-04 16:43:30 -0600

    Update Coast.jsx

--- src/Components/Coast/Coast.jsx
+++ src/Components/Coast/Coast.jsx
@@ -1,40 +1,7 @@
 import React, { useState } from 'react';
-import slogo from '../../assets/s_logo.png';
-
 import CoastPrototype from './CoastPrototype';
 
-// i imported all of my sections of my portfolio
-// separately as components 
-// all have prototypes in them
-
-
 export default function Coast () {
-
-    const [showBtn, setShowBtn] = useState("myBtn none");
-
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function () {
-      scrollFunction();
-    };
-  
-    function scrollFunction() {
-      if (
-        document.body.scrollTop > 20 ||
-        document.documentElement.scrollTop > 20
-      ) {
-        setShowBtn("myBtn");
-      } else {
-        setShowBtn("none");
-      }
-    }
-  
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    }
-
-
 
     return (
       <div className="flex flex-col w-full">
@@ -43,7 +10,7 @@ export default function Coast () {
 
       <div className="flex flex-col w-full h-screen justify-center" >
 
-          <div className="flex justify-center py-80 ">
+          <div className="flex flex-col justify-center py-96 ">
             <div className="sm:text-4xl lg:text-8xl font-coast justify-center ">COAST</div>
             <div className="flex justify-center text-sm font-sans w-36 bg-black bg-opacity-20 border-black border-2 rounded-lg px-4">UX/UI Case Study</div>
           </div>
@@ -55,12 +22,6 @@ export default function Coast () {
     </div>
 
 
-
-        <div>
-          <button onClick={topFunction} id="myBtn" className={showBtn} title="Go to top" >
-            top
-          </button>
-        </div>
 </div>
     );
 };
