7160a46cb3d55cf3e4e3f29514b0998fe647a713
alishafulton <af.ui.design@gmail.com>
2025-01-05 17:01:06 -0600

    Update Coast.jsx

--- src/Components/Coast/Coast.jsx
+++ src/Components/Coast/Coast.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import CoastPrototype from './CoastPrototype';
+import Problem from './Problem';
 
 export default function Coast () {
 
