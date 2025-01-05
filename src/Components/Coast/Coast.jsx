326d2dbfbc055e3b468f9e636d1a2563039dd8c4
alishafulton <af.ui.design@gmail.com>
2025-01-05 17:42:46 -0600

    Update Coast.jsx

--- src/Components/Coast/Coast.jsx
+++ src/Components/Coast/Coast.jsx
@@ -6,14 +6,16 @@ export default function Coast () {
 
     return (
         <div className="flex w-full pt-2 justify-center bg-coast-logo bg-scroll bg-contain bg-top overflow-auto bg-no-repeat">
-          <div className="flex flex-col w-full h-screen py-12" >
+          <div className="flex flex-col w-full h-screen" >
 
             <div className="flex flex-col justify-center py-44 lg:py-84 space-y-4">
-              <div className="text-6xl lg:text-9xl font-coast flex justify-center ">COAST</div>
+
+               <div className="text-6xl lg:text-9xl font-coast flex justify-center">COAST</div>
               
-              <div className="flex justify-center">
-                <div className="flex justify-center text-sm rounded-xl font-sans w-32 bg-black text-white px-2 py-1">UX/UI Case Study</div>
-              </div>
+               <div className="flex justify-center">
+                 <div className="flex justify-center text-sm rounded-xl font-sans w-32 bg-black text-white px-2 py-1">UX/UI Case Study</div>
+               </div>
+
             </div>
 
             <div className="flex flex-col justify-center w-full py-24">
@@ -24,7 +26,7 @@ export default function Coast () {
                 a streamlined, stress-free process with clarity at every step. </div></div>
             </div>
 
-            <div className="flex flex-col justify-center w-full py-24 mx-4 px-4">
+            <div className="flex flex-col justify-center w-full py-24px-4">
 
               <div className="flex justify-center font-coast text-2xl p-3 w-full">PROBLEM</div>
               
