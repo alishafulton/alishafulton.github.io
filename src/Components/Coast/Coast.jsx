1a7e5585fdcec5600b5f83b900517b233a130366
alishafulton <af.ui.design@gmail.com>
2025-01-05 12:58:49 -0600

    Update Coast.jsx

--- src/Components/Coast/Coast.jsx
+++ src/Components/Coast/Coast.jsx
@@ -10,9 +10,18 @@ export default function Coast () {
           <div className="flex flex-col justify-center py-44 lg:py-84 space-y-4">
             <div className="text-6xl lg:text-9xl font-coast flex justify-center ">COAST</div>
             <div className="flex justify-center">
-              <div className="flex justify-center text-sm  font-sans w-32 bg-black text-white px-2 py-1">UX/UI Case Study</div>
+              <div className="flex justify-center text-xs rounded font-sans w-32 bg-black text-white px-2 py-1">UX/UI Case Study</div>
             </div>
             </div>
+
+            <div className="flex flex-col py-6 justify-center">
+              <div className="font-coast text-lg p-3">ABOUT</div>
+              <div className="font-coast text-sm justify-center">
+                Coast is a native app for both iOS and Android users that provides an 
+                online marketplace for short-term home rentals. It's goal is to equip users with
+                a streamlined, stress-free process with clarity at every step. </div>
+            </div>
+
 
           <div className="flex w-full justify-center" data-aos="slide-up"><CoastPrototype/></div>
           </div>
