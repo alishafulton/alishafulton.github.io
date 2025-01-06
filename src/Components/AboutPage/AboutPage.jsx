1110e15cd4f8fe6846f1f078d17773cd4570460f
alishafulton <af.ui.design@gmail.com>
2025-01-06 16:12:58 -0600

    v

--- src/Components/AboutPage/AboutPage.jsx
+++ src/Components/AboutPage/AboutPage.jsx
@@ -7,9 +7,10 @@ import Compliments from "./Compliments";
 export default function AboutPage() {
     
       return (
-        <div className="flex flex-col justify-center justify-items-center content-start w-full h-full ">
+        <div>
+        <div className="flex flex-col justify-center justify-items-center content-start w-full h-full">
             <Blurb />
             <Compliments/>
-        </div>
+        </div></div>
       );
     }
