c27a1288ee4388167934751cb753fa02eb786428
alishafulton <af.ui.design@gmail.com>
2025-01-05 21:13:09 -0600

    Update Problem.jsx

--- src/Components/Coast/Problem.jsx
+++ src/Components/Coast/Problem.jsx
@@ -22,8 +22,8 @@ export default function Problem () {
  <div className="flex flex-col w-full  justify-center justify-items-center p-2">
 
         <div className="flex flex-col w-full py-2">
-            <div className="flex w-full bg-black text-white rounded-lg text-center justify-center py-2 px-4 font-coast">Time-consuming Search Process</div>
-            <div className="text-sm font-serif bg-white ">skifhuisjhre owiejr oij ioj oi kjxkck sopspepele. i if i oeoefm  </div>
+            <div className="flex w-full bg-black text-white rounded-lg text-center justify-center py-2 px-4 font-coast">Time-Consuming Search Process</div>
+            <div className="text-sm font-serif bg-white p-5 h-auto">Lorem ipsum dolor sit amet consectetur. Etiam sed congue viverra consectetur ipsum integer egestas laoreet. Nisi nam convallis massa facilisis molestie in. Etiam in ac non est. </div>
             </div>
     </div>
 
