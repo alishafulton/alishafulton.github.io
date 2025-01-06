44f3eb91970775fb98ccdab8b06d1c14956ace2e
alishafulton <af.ui.design@gmail.com>
2025-01-05 20:07:12 -0600

    Update Problem.jsx

--- src/Components/Coast/Problem.jsx
+++ src/Components/Coast/Problem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import emotion1 from './../../assets/coast_emotion1.png';
 import emotion2 from './../../assets/coast-emotion2.png';
-import emotion2 from './../../assets/coast_emotion3.png';
+import emotion3 from './../../assets/coast_emotion3.png';
 
 
 export default function Problem () {
@@ -10,6 +10,16 @@ export default function Problem () {
 <div>
  
  <div className="w-full justify-center grid grid-cols-3 gap-4 p-4 my-4">
+
+ <div className="flex flex-col w-full  justify-center justify-items-center p-2">
+        <img src={ emotion2 } className="max-w-60 justify-center" />
+
+        <div className="flex flex-col w-full bg-white">
+            <div className="flex w-full text-center justify-center py-2 font-coast">Unreliable reviews</div>
+            <div className="text-sm font-serif ">skifhuisjhre owiejr oij ioj oi kjxkck sopspepele. i if i oeoefm  </div>
+            </div>
+    </div>
+    
  <div className="flex flex-col w-full  justify-center justify-items-center p-2">
         <img src={ emotion3 } className="max-w-60 justify-center" />
 
@@ -19,14 +29,7 @@ export default function Problem () {
             </div>
     </div>
 
-    <div className="flex flex-col w-full  justify-center justify-items-center p-2">
-        <img src={ emotion2 } className="max-w-60 justify-center" />
-
-        <div className="flex flex-col w-full bg-white">
-            <div className="flex w-full text-center justify-center py-2 font-coast">Unreliable reviews</div>
-            <div className="text-sm font-serif ">skifhuisjhre owiejr oij ioj oi kjxkck sopspepele. i if i oeoefm  </div>
-            </div>
-    </div>
+   
 
     <div className="flex flex-col w-full  justify-center justify-items-center p-2">
         <img src={ emotion1 } className="max-w-60 justify-center" />
