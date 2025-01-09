d5875fd7a36b03996460c7ef7ce0578abb583278
alishafulton <af.ui.design@gmail.com>
2025-01-09 14:40:51 -0600

    Update Channel.jsx

--- src/Components/Channel/Channel.jsx
+++ src/Components/Channel/Channel.jsx
@@ -58,7 +58,7 @@ export default function Channel () {
 
           <div className="flex flex-col place-content-end">
             <div className="flex w-full text-right text-3xl place-content-end">solution</div>
-            <div className="text-lg text-right w-2/3 place-content-end">To provide a space for healthcare workers to review their environments, and for those reviews to assist healthcare workers who are looking to switch facilities.</div>
+            <div className="text-lg text-right w-full place-content-end">To provide a space for healthcare workers to review their environments, and for those reviews to assist healthcare workers who are looking to switch facilities.</div>
         </div>
         </div>
 
