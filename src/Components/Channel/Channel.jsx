e2917695a5374cc837dac884b53dbc9daebca79f
alishafulton <af.ui.design@gmail.com>
2025-01-09 18:52:13 -0600

    Update Channel.jsx

--- src/Components/Channel/Channel.jsx
+++ src/Components/Channel/Channel.jsx
@@ -64,8 +64,8 @@ export default function Channel () {
         <img src={ channel1 } alt="" className="py-4 flex w-500" />
 
         
-        <div className="flex w-2/3 place-self-center text-white font-channel static">
-          <div className="flex w-500 h-500 bg-white bg-opacity-20 rounded-lg z-10 absolute justify-center items-center "></div>
+        <div className="flex w-2/3 place-self-center text-white font-channel relative">
+          <div className="flex w-500 h-500 bg-white bg-opacity-20 rounded-lg z-10 absolute "></div>
 
             <div className="grid grid-cols-3 space-x-2 z-20 relative">
 
