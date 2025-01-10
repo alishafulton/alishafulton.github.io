97d8db92cb16aa83c39ab6eeb1808edc304efb9a
alishafulton <af.ui.design@gmail.com>
2025-01-10 09:41:50 -0600

    Update Channel.jsx

--- src/Components/Channel/Channel.jsx
+++ src/Components/Channel/Channel.jsx
@@ -65,7 +65,7 @@ export default function Channel () {
 
         
         <div className="flex w-2/3 place-self-center text-white font-channel relative">
-          <div className="flex w-500 h-500 bg-white bg-opacity-20 rounded-lg z-10 absolute place-self-center"></div>
+          <div className="flex w-500 h-500 bg-white bg-opacity-20 rounded-lg z-10 absolute place-self-center justify-center"></div>
 
             <div className="grid grid-cols-3 space-x-2 z-20 relative">
 
