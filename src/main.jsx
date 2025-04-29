ce34e9e6651fe444ead40a49c0bd98ac2ab51cb7
alishafulton <af.ui.design@gmail.com>
2025-04-29 10:29:35 -0500

    React Router?

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const Layout =()=> {
+  return(
+    <div>
+      <Outlet />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([{
+  path:'/',
+  element: <Layout/>,
+  children:[
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+      path: "/about",
+      element: <App />,
+    }
+]
+}])
+
+ReactDom.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 )
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>About Meeeeeee</div>
+  )
+}
+
+export default About
