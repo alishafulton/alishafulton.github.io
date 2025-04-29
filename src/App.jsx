021e881507b669e0b9a754aec49b5d4ebdd584da
alishafulton <af.ui.design@gmail.com>
2025-04-29 10:37:51 -0500

    Try 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <h1 className="text-lg font-mono">
-- 2025
-</h1>
-        </div>
-    </>
-  )
+    <div>
+      Hello
+    </div>
+  );
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Outlet, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter } from 'react-router-dom';
@@ -22,11 +23,11 @@ const router = createBrowserRouter([{
     },
     {
       path: "/about",
-      element: <App />,
+      element: <About />,
     }
 ]
 }])
 
-ReactDom.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 )
