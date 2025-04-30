794d70461e4b42c743159753389710f74247ab35
alishafulton <af.ui.design@gmail.com>
2025-04-30 09:45:55 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react'
 import About from './pages/ABout';
 
+
 function App () {
   return <BrowserRouter>
   <Routes>
-    <Route path="/" element={<About/>} />
+    <Route path="/" element={<About />} />
   </Routes>
 </BrowserRouter>;
 }
