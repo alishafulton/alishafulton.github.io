8cf708131283fd33e35da8fd521afe84fe56a076
alishafulton <af.ui.design@gmail.com>
2025-04-30 11:30:18 -0500

    Try 34521

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react'
 import Home from './pages/Home';
-import About from './pages/About';
+import About from './pages/ABout';
 
 
 function App () {
