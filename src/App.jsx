472ce566cde5843fa5818e66e88a90e0e5d8adce
alishafulton <af.ui.design@gmail.com>
2025-04-30 11:19:32 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react'
 import Home from './pages/Home';
 
 
 function App () {
-  return 
-    <Home/>
-  ;
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={ <Home/> }/>
+    </Routes>
+    </BrowserRouter> ;
 }
 
 export default App;
