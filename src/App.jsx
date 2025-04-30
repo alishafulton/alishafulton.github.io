a3c71d2a51de0a6de31d2fb737ef8cea3484cbd8
alishafulton <af.ui.design@gmail.com>
2025-04-30 09:47:53 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react'
-import About from './pages/ABout';
+import About from '.././src/pages/ABout';
 
 
 function App () {
