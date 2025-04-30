15ae1a5a97a786a70faad89553ff795a9fb8c99f
alishafulton <af.ui.design@gmail.com>
2025-04-30 15:12:35 -0500

    oops

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,14 @@ import React from "react";
 import './index.css'
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
-import AboutPage from "./components/AboutPage";
 
 export default function App () {
   return (
     <div>
-            <HashRouter>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                </ul>
-            </nav>
             <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/about" element={<AboutPage />} />
+              <Route path='/' element={<HomePage/>}/>
+              <Route path='/about' element={<AboutPage/>}/>
             </Routes>
-        </HashRouter>
       </div>
 
     
