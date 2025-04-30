837723802b8de0e030ed1032282e1784ef1091c9
alishafulton <af.ui.design@gmail.com>
2025-04-30 11:40:43 -0500

    rfjy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react'
 import Home from './pages/Home';
+import About from './pages/ABout';
 
 
 function App () {
-  return <BrowserRouter>
+  return <BrowserRouter basename={import.meta.env.BASE_URL} >
     <Routes>
       <Route path="/" element={ <Home/> }/>
+      <Route path="/" element={ <About/> }/>
     </Routes>
     </BrowserRouter> ;
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+
+
+
+function About () {
+  return <>
+ about
+  </>;
+}
+
+export default About;
