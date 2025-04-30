5561f9d033e3b60d313a5590365264961b6fbe54
alishafulton <af.ui.design@gmail.com>
2025-04-30 11:33:23 -0500

    -1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react'
 import Home from './pages/Home';
-import About from './pages/ABout';
 
 
 function App () {
