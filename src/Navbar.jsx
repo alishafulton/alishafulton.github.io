116bcde82e8ba68f45d8990e517348c74329e2b9
alishafulton <af.ui.design@gmail.com>
2025-05-07 15:26:54 -0500

    Take 34

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <div className="App" class="py-3 px-20 flex w-full font-serif justify-end">
+        <div className="App" class="py-3 px-20 flex w-full font-serif text-sm justify-end">
                     <Link to="/" class="p-4 hover:text-pink-300 active:text-pink-800">Home</Link>
                     <Link to="/about" class="p-4 hover:text-pink-300 active:text-pink-800">About Me</Link>
                     <Link to="/portfolio" class="p-4 hover:text-pink-300 active:text-pink-800">Portfolio</Link>
--- src/components/Biome.jsx
+++ src/components/Biome.jsx
@@ -29,7 +29,7 @@ function BiomePage () {
         transition={{ delay: 0.5, duration: 1.1, ease: 'easeOut' }}
         viewport={{ once: true }}
     class="w-3/5 py-16">
-        <div class="text-8xl font-castio">biome</div>
+        <div class="text-5xl lg:text-8xl font-castio">biome</div>
         <div class="text-xl lg:text-2xl pb-6">_________________________________</div>
         <div class="font-outfit text-sm lg:text-lg xl:text-md w-4/5">
             Biome represents a design concept for a healthcare-focused social 
