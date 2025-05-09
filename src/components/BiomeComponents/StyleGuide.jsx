b61f6499de41c1b0428f4ed4ac910ed01beef463
alishafulton <af.ui.design@gmail.com>
2025-05-08 20:16:04 -0500

    Update StyleGuide.jsx

--- src/components/BiomeComponents/StyleGuide.jsx
+++ src/components/BiomeComponents/StyleGuide.jsx
@@ -15,7 +15,7 @@ function StyleGuide () {
 
         <div class="flex w-full h-full absolute top-0 right-0">
             <div class="flex w-full h-full place-content-center">
-                <div class="flex flex-col w-5/6 h-full py-10 lg:py-20 place-self-center justify-between">
+                <div class="flex flex-col w-5/6 h-full py-6 lg:py-20 place-self-center justify-between">
                     <div class="grid grid-cols-4 gap-4 lg:gap-10 w-full h-5/12 ">
                         <div class="flex flex-col gap-4 lg:gap-10">
                             <div class="rounded-xl lg:rounded-3xl bg-[#FAFAFA] text-black content-end p-2 lg:p-4 font-outfit text-[9px] lg:text-sm h-full drop-shadow-xl/20">#FAFAFA</div>
